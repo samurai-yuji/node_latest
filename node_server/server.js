b650f12ac5d80bea95ba8fc99c8eb2c8d828dc2f
Yuji Nishida <yuji.nishida@example.com>
2018-09-05 19:55:14 +0900

    Removed useless module and functions.

--- node_server/server.js
+++ node_server/server.js
@@ -5,7 +5,6 @@ let Router = require("router");
 let finalHandler = require("finalHandler");
 let bodyParser = require("body-parser");
 
-let url = require("url");
 let fs = require("fs");
 
 let requestHandlers = require("./requestHandlers");
@@ -13,18 +12,13 @@ let requestHandlers = require("./requestHandlers");
 function server_start(route, handle) {
 
   var router = Router();
-
   router.use(bodyParser());
 
   router.get("/",requestHandlers.start);
   router.post("/upload",requestHandlers.upload);
 
-  function onRequest(request, response) {
-    router(request, response, finalHandler(request, response));
-  }
-
   http.createServer((req, res) => {
-    res.writeHead(301,{Location: "https://localhost:8888"});
+    res.writeHead(301,{Location: "https://localhost:8443"});
     res.end();
   }).listen(8080,'0.0.0.0');
 
@@ -33,9 +27,10 @@ function server_start(route, handle) {
     cert: fs.readFileSync('server.crt')
   };
 
-  https.createServer(options,onRequest).listen(8888,'0.0.0.0');
+  https.createServer(options,(req, res) => {
+    router(req, res, finalHandler(req, res));
+  }).listen(8443,'0.0.0.0');
   console.log("Server has started.");
-
 }
 
 server_start();
