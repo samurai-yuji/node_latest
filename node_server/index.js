eea8e880673366ac8f6feda8e472dc8af956ff6b
Yuji Nishida <yuji.nishida@example.com>
2018-09-05 19:56:17 +0900

    Removed useless files, too.

--- node_server/index.js
+++
@@ -1,9 +0,0 @@
-let requestHandlers = require("./requestHandlers"),
-server = require("./server"),
-router = require("./router");
-
-let handle = {};
-handle["/"] = requestHandlers.start;
-handle["/upload"] = requestHandlers.upload;
-
-server.start(router.route, handle);
